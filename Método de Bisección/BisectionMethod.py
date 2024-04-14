ca59b0bbdbd0d33090dca9d8498c9d44d9bc23a9
CristopherAlejandoRuizBouchez <100699422+CristopherAlejandoRuizBouchez@users.noreply.github.com>
2024-04-13 19:06:03 -0600

    Add files via upload

---
+++ Método de Bisección/BisectionMethod.py
@@ -0,0 +1,136 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class BisectionSearch:
+    def __init__(self, func, lower_bound, upper_bound):
+        self.func = func
+        self.lower_bound = lower_bound
+        self.upper_bound = upper_bound
+
+    def search(self, precision):
+        iterations = []
+        a = self.lower_bound
+        b = self.upper_bound
+
+        while abs(b - a) > precision:
+            c = (a + b) / 2
+            if self.func(c) == 0:
+                break
+            elif self.func(a) * self.func(c) < 0:
+                b = c
+            else:
+                a = c
+            iterations.append(c)
+
+        return iterations
+
+def f1(x):
+    return x**2 + 54/x
+
+def f2(x):
+    return x**3 + 2*x - 3
+
+def f3(x):
+    return x**4 + x**2 - 33
+
+def f4(x):
+    return 3*x**4 - 8*x**3 - 6*x**2 + 12*x
+
+def caja(L):
+    return (L * (20 - 2*L) * (10 - 2*L))*-1
+
+def lata(x):
+    return 2 * np.pi * x ** 2 + (500 / x)
+
+search_f1 = BisectionSearch(f1, 0.1, 10)
+search_f2 = BisectionSearch(f2, -5, 5)
+search_f3 = BisectionSearch(f3, -2.5, 2.5)
+search_f4 = BisectionSearch(f4, -1.5, 3)
+search_caja = BisectionSearch(caja, 2, 3)  
+search_lata = BisectionSearch(lata, 0.1, 10)
+
+precision_values = [0.5, 0.1, 0.01, 0.0001]
+
+for precision in precision_values:
+    plt.figure(figsize=(15, 10))
+
+    # f1
+    plt.subplot(2, 3, 1)
+    iterations = search_f1.search(precision)
+    x_values = np.linspace(0.01, 10, 1000)
+    y_values = f1(x_values)
+    plt.plot(x_values, y_values, label=f'Precision = {precision}')
+    plt.scatter(iterations, [f1(x) for x in iterations], color='red', label=f'Iterations (Precision: {precision})')
+    plt.xlabel('x')
+    plt.ylabel('f(x)')
+    plt.title('f1(x) = x^2 + 54/x')
+    plt.legend()
+    plt.grid(True)
+
+    # f2
+    plt.subplot(2, 3, 2)
+    iterations = search_f2.search(precision)
+    x_values = np.linspace(-5, 5, 1000)
+    y_values = f2(x_values)
+    plt.plot(x_values, y_values, label=f'Precision = {precision}')
+    plt.scatter(iterations, [f2(x) for x in iterations], color='red', label=f'Iterations (Precision: {precision})')
+    plt.xlabel('x')
+    plt.ylabel('f(x)')
+    plt.title('f2(x) = x^3 + 2x - 3')
+    plt.legend()
+    plt.grid(True)
+
+    # f3
+    plt.subplot(2, 3, 3)
+    iterations = search_f3.search(precision)
+    x_values = np.linspace(-2.5, 2.5, 1000)
+    y_values = f3(x_values)
+    plt.plot(x_values, y_values, label=f'Precision = {precision}')
+    plt.scatter(iterations, [f3(x) for x in iterations], color='red', label=f'Iterations (Precision: {precision})')
+    plt.xlabel('x')
+    plt.ylabel('f(x)')
+    plt.title('f3(x) = x^4 + x^2 - 33')
+    plt.legend()
+    plt.grid(True)
+
+    # f4
+    plt.subplot(2, 3, 4)
+    iterations = search_f4.search(precision)
+    x_values = np.linspace(-1.5, 3, 1000)
+    y_values = f4(x_values)
+    plt.plot(x_values, y_values, label=f'Precision = {precision}')
+    plt.scatter(iterations, [f4(x) for x in iterations], color='red', label=f'Iterations (Precision: {precision})')
+    plt.xlabel('x')
+    plt.ylabel('f(x)')
+    plt.title('f4(x) = 3x^4 - 8x^3 - 6x^2 + 12x')
+    plt.legend()
+    plt.grid(True)
+
+    # lata
+    plt.subplot(2, 3, 5)
+    iterations = search_lata.search(precision)
+    x_values = np.linspace(0.01, 10, 1000)
+    y_values = lata(x_values)
+    plt.plot(x_values, y_values, label=f'Precision = {precision}')
+    plt.scatter(iterations, [lata(x) for x in iterations], color='red', label=f'Iterations (Precision: {precision})')
+    plt.xlabel('Radio (x)')
+    plt.ylabel('Área de Superficie')
+    plt.title('Lata')
+    plt.legend()
+    plt.grid(True)
+
+    # caja
+    plt.subplot(2, 3, 6)
+    iterations = search_caja.search(precision)
+    L_values = np.linspace(2, 3, 1000)
+    caja_values = caja(L_values)
+    plt.plot(L_values, caja_values, label=f'Precision = {precision}')
+    plt.scatter(iterations, [caja(x) for x in iterations], color='red', label=f'Iterations (Precision: {precision})')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.title('Caja')
+    plt.legend()
+    plt.grid(True)
+
+    plt.tight_layout()
+    plt.show()
---
+++ Método de Bisección/funcion_caja.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def f(L):
+    return (L * (20 - 2*L) * (10 - 2*L))*-1
+
+def bisection_search(func, lower_bound, upper_bound, precision):
+    iterations = []
+    a = lower_bound
+    b = upper_bound
+
+    while abs(b - a) > precision:
+        c = (a + b) / 2
+        if func(c) == 0:
+            break
+        elif func(a) * func(c) < 0:
+            b = c
+        else:
+            a = c
+        iterations.append(c)
+
+    return iterations
+
+
+precisiones = [0.5, 0.1, 0.01, 0.0001]
+
+valores_x = np.linspace( 2, 3, 1000)
+valores_y = f(valores_x)
+for precision in precisiones:
+    plt.figure()
+    iterations = bisection_search(f,  2, 3, precision)
+    plt.plot(valores_x, valores_y, label='f(x)')
+    plt.scatter(iterations, [f(x) for x in iterations], color='red', label='Visited Points')
+    plt.title(f'bisection_search (Precision: {precision})')
+    plt.xlabel('x')
+    plt.ylabel('f(x)')
+    plt.legend()
+    plt.grid()
+    plt.show()
